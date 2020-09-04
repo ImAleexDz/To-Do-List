3c4946104cc06f02225d2fb52e19575416224c83
ImAleexDz <alexndro7145@gmail.com>
2020-09-03 22:29:23 -0500

    add Navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-            <a className="navbar-brand" href="#">To-do List</a>
+        <nav className="navbar navbar-expand-lg navbar-light my-navbar">
+            <a className="navbar-brand" href="#">To Do List</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Add Task <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="#">+ Add Task <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">About</a>
